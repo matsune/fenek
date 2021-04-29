6412c6b22c0be1d020b53a13373ce0f5e0d154ea
Yuma Matsune <yuma.matsune@gmail.com>
2021-04-29 12:38:30 +0900

    removed unused package

---
+++ compiler/pos/src/src_file.rs
@@ -0,0 +1,39 @@
+use super::{Offset, Pos};
+use std::convert::TryInto;
+use std::io::prelude::Read;
+use std::path::PathBuf;
+use std::str::Chars;
+
+pub struct SrcFile {
+    pub path: PathBuf,
+    inner: String,
+}
+
+impl SrcFile {
+    pub fn open<P: Into<PathBuf>>(path: P) -> std::io::Result<SrcFile> {
+        let path = path.into();
+        let mut src_file = std::fs::File::open(&path)?;
+        let mut buf = String::new();
+        src_file.read_to_string(&mut buf)?;
+        Ok(SrcFile { path, inner: buf })
+    }
+
+    pub fn pos_from_offset(&self, offset: Offset) -> Pos {
+        let mut pos = Pos::default();
+        for (idx, c) in self.inner.chars().enumerate() {
+            if idx == offset as usize {
+                break;
+            }
+            if c == '\n' || c == '\r' {
+                pos.newline();
+            } else {
+                pos.add_row(c.len_utf16().try_into().unwrap());
+            }
+        }
+        pos
+    }
+
+    pub fn chars(&self) -> Chars<'_> {
+        self.inner.chars()
+    }
+}
